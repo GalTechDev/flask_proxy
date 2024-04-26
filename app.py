f9394f481bcca36d3467f0475cf5edb9f2a1f9c1
GalTechDev <poussigalitv@gmail.com>
2024-04-26 15:11:43 +0200

    add websocket suport

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import request, Response, Flask
+from flask_sock import Sock
 import requests
 import json
+import websocket
+import rel
 
 with open("config.json") as f:
     config = json.load(f)
 
 app = Flask('__main__')
-app.config['SERVER_NAME'] = f'{config.get("subdomain")}.{config.get("domain")}'
-
+app.config['SERVER_NAME'] = f'{config.get("domain")}'
+socketio = Sock(app)
 ref = config.get("redirection")
 
-@app.route('/') 
+@app.route('/', subdomain=config.get("subdomain")) 
 def home():
     page = ""
     for name in ref.keys():
@@ -18,7 +21,42 @@ def home():
     return page
 
 methods = ["GET", "POST", "HEAD", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]
-def register(subdomain, sitename):
+def register(subdomain, sitename:str):
+
+    @socketio.route("/ws", subdomain=subdomain)
+    def handle_connect(ws):
+
+        def on_message(sws, message):
+            ws.send(message)
+
+        def on_error(sws, error):
+            print(error)
+
+        def on_close(sws, close_status_code, close_msg):
+            ws.close()
+
+        def on_open(sws):
+            print("Server connected")
+
+        print('Client connected')
+        # Ouvrir une connexion WebSocket avec le serveur distant
+        ws_url = f"ws://{sitename.removeprefix('http://').removeprefix('https://')}/ws"
+        sws = websocket.WebSocketApp(ws_url,
+                                    on_open=on_open,
+                                    on_message=on_message,
+                                    on_error=on_error,
+                                    on_close=on_close)
+        
+        sws.run_forever()
+        while True:
+            try:
+                client_data = ws.recv()
+                sws.send(client_data)
+            except Exception:
+                sws.close()
+                ws.close()
+                break
+
     @app.route('/', defaults={'path': ''}, methods=methods, subdomain=subdomain, endpoint=subdomain) 
     @app.route('/<path:path>', methods=methods, subdomain=subdomain, endpoint=subdomain)
     @app.route('/static/<path:path>', methods=methods, subdomain=subdomain, endpoint=subdomain)
@@ -60,4 +98,4 @@ def register(subdomain, sitename):
 if __name__ == '__main__':
     for sub, site in ref.items():
         register(sub, site)
-    app.run(host=config.get("ip"), port=config.get("port"))
+    app.run(host=config.get("ip"), port=config.get("port"), debug=True)
